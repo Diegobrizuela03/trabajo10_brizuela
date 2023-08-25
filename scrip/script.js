005f7e559206483814d6cb8aca8b2e9fad01afdf
Diegobrizuela03 <140198071+Diegobrizuela03@users.noreply.github.com>
2023-08-24 21:22:57 -0300

    Add files via upload

---
+++ scrip/script.js
@@ -0,0 +1,20 @@
+var nombre="Diego";
+var apellido="Brizuela";
+
+console.log("Bienvenido" + " " + nombre + " " + apellido)
+
+
+
+var numero1= 2;
+var numero2=1;
+var suma= numero1 + numero2; 
+
+console.log("El resultado de la suma es " + suma)
+
+
+var numero3=7;
+var resultado= suma + numero3;
+
+console.log("La suma es" + " " + resultado)
+
+
